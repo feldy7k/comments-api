aeeb95f5698ffc8849e15ddab739293bb0f86577
feldy7k <feldy7k@outlook.com>
2024-06-05 01:44:43 +0700

    modify nested field

--- CommentsAPI/CommentsAPI/Model/CommentModel.cs
+++ CommentsAPI/CommentsAPI/Model/CommentModel.cs
@@ -6,7 +6,15 @@ namespace CommentsAPI.Model
     {
         [Required] // triggered when ModelState.IsValid
         [StringLength(1000, MinimumLength = 1)] // triggered when ModelState.IsValid
-        [SanitizeInput] // triggered when ModelState.IsValid
+        [SanitizeInput("Content")] // triggered when ModelState.IsValid
         public string Content { get; set; }
+
+        public CommentDetail CommentDetail { get; set; }
+    }
+
+    public class CommentDetail
+    {
+        [SanitizeInput("Author")] // triggered when ModelState.IsValid
+        public string Author { get; set; }
     }
 }
--- CommentsAPI/CommentsAPI/Model/SanitizeInput.cs
+++ CommentsAPI/CommentsAPI/Model/SanitizeInput.cs
@@ -1,22 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.Extensions.Primitives;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace CommentsAPI.Model
 {
     public class SanitizeInput : ValidationAttribute
     {
-        public SanitizeInput()
+        private string _propertyName = "";
+        public SanitizeInput(string propertyName)
         {
-
+            _propertyName = propertyName;
         }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            // modifying the property value
-            var instance = validationContext.ObjectInstance as CommentModel;
-            if (instance != null)
+            string stringValue = value as string;
+
+            // Get the property info of the nested property
+            PropertyInfo property = validationContext.ObjectType.GetProperty(_propertyName);
+            if (property == null)
             {
-                instance.Content = HtmlSanitizer.Sanitize(instance.Content); // Sanitize the value
+                return new ValidationResult($"Property '{_propertyName}' not found.");
             }
+
+            // Modify the property value
+            property.SetValue(validationContext.ObjectInstance, HtmlSanitizer.Sanitize(stringValue), null);
+
 
             // validation is successful
             return ValidationResult.Success;
